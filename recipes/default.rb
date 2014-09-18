4040c8649b49c9889a20c1d0652923b8dfd5f11b
y2bishop2y <emiliano@medlista.com>
2014-09-18 10:55:52 -0700

    Initial checkin

---
+++ recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: wonderstuff
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'lighttpd'
+
+service 'lighttpd' do 
+  action [:enable, :start]
+end
+
+
+cookbook_file '/var/www/index.html' do
+  source 'wonderstuff.html'
+  owner 'www-data'
+  group 'www-data'
+end
---
+++ spec/unit/default_spec.rb
@@ -0,0 +1,32 @@
+# require 'rspec'
+require 'chefspec'
+
+describe 'wonderstuff::default' do
+
+  let(:chef_run) do
+    runner = ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04' )
+    
+    Chef::Config.force_logger true
+    runner.converge('recipe[wonderstuff::default]')
+  end
+
+  
+  it "installs the lighttpd package" do
+    expect(chef_run).to install_package 'lighttpd'
+  end
+  
+
+  it "creates a webpage to be served" do
+    content = "wonderstuff Design is a boutique graphics design agency"
+    expect(chef_run).to render_file('/var/www/index.html').with_content(content)
+    expect(chef_run).to create_cookbook_file('/var/www/index.html').with(owner: 'www-data', group: 'www-data')
+  end
+
+  it "start the lighttpd service" do
+    expect(chef_run).to start_service 'lighttpd'
+  end
+
+  it "enables the lighttpd service" do
+    excpect(chef_run).to enable_service 'lighttpd'
+  end
+end
